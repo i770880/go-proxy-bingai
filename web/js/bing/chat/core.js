4fa843558d8a2ac3f30ef1346538a71d743dac84
i770880 <141414410+i770880@users.noreply.github.com>
2023-08-18 23:21:44 +0800

    Update core.js

--- web/js/bing/chat/core.js
+++ web/js/bing/chat/core.js
@@ -13,7 +13,7 @@ _w.rms.js(
   { 'A:rms:answers:Shared:BingCore.Bundle': '/rp/Pki1-YEXD6vos5MiDyyAeDq7sgs.br.js' },
   { 'A:rms:answers:Web:SydneyFSCHelper': '/rp/plViotfzKXF_pqqXx4NWn6aW_II.br.js' },
   { 'A:rms:answers:VisualSystem:ConversationScope': '/rp/YFRe970EMtFzujI9pBYZBGpdHEo.br.js' },
-//  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/h_z8hgmoir53hvshQQoyF7vDrYc.br.js' },
+  { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/h_z8hgmoir53hvshQQoyF7vDrYc.br.js' },
   { 'A:rms:answers:SharedStaticAssets:speech-sdk': '/rp/6slp3E-BqFf904Cz6cCWPY1bh9E.br.js' },
   { 'A:rms:answers:Web:SydneyWelcomeScreenBase':'/rp/tAJNECpUpSdBSxOEHv5jZ0nF93I.br.js' },
   { 'A:rms:answers:Web:SydneyWelcomeScreen':'/rp/H-Z1_dXpX8YW6Di4p6iwWmTVvMs.br.js' },
